8fa7113f9f059de96d140260fb4c437dab73e47c
cosmah <ssekirya.cosma@student.utamu.ac.ug>
2023-02-22 17:04:31 +0300

    menu responsiveness perfected

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
-const navbarToggle = document.querySelector('.navbar-toggle');
-const navbarMenu = document.querySelector('.navbar-menu');
-
-navbarToggle.addEventListener('click', () => {
-  navbarToggle.classList.toggle('active');
-  navbarMenu.classList.toggle('active');
-});
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
